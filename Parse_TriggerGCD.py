3aace26232681e249bee747e00e4ed64309260b3
Aethys256 <aethys256@users.noreply.github.com>
2017-05-21 14:52:55 +0200

    [Parse] Trigger GCD Release

---
+++ Parse_TriggerGCD.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=C0103
+# pylint: disable=C0301
+
+"""
+@author: Aethys256
+"""
+
+import sys
+import os
+import csv
+
+generatedDir = os.path.join('DBC', 'generated')
+parsedDir = os.path.join('DBC', 'parsed')
+
+os.chdir(os.path.join(os.path.dirname(sys.path[0]), 'AethysDBC'))
+with open(os.path.join(generatedDir, 'SpellCooldowns.csv')) as csvfile:
+    reader = csv.DictReader(csvfile)
+    with open(os.path.join(parsedDir, 'TriggerGCD.lua'), 'w', encoding='utf-8') as file:
+        file.write('AethysCore.Enum.TriggerGCD = {\n')
+        for row in reader:
+            file.write('  [' + row['id_spell'] + '] = ' + ('true' if int(row['gcd_cooldown']) > 0 else 'false') + ',\n')
+        file.write('}\n')
