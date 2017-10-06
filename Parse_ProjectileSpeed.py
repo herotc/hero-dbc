7a054009073f0c85925ec94dba7255ee86416e05
Aethys256 <aethys256@users.noreply.github.com>
2017-10-06 03:34:52 +0200

    [Parse] ProjectileSpeed comments part2

--- Parse_ProjectileSpeed.py
+++ Parse_ProjectileSpeed.py
@@ -15,15 +15,18 @@ parsedDir = os.path.join('DBC', 'parsed')
 
 os.chdir(os.path.join(os.path.dirname(sys.path[0]), 'AethysDBC'))
 
-# Since Blizzard stores the Projectile Speed in SpellMisc and that SpellMisc doesn't contain the id_spell but only the id_misc
-# we have to parse Spell.db to map the id_spell & id_misc.
+## Mapping
+# id_spell & id_misc from Spell
+# id_misc & proj_speed from SpellMisc
+
+# Get every misc id associated to every spell id
 Spells = {}
 with open(os.path.join(generatedDir, 'Spell.csv')) as csvfile:
     reader = csv.DictReader(csvfile, escapechar='\\')
     for row in reader:
         Spells[row['id_misc']] = row['id']
 
-
+# Variables to compute the Projectile Speed Mean, for fun =)
 PrjSpeedRawValue = 0
 PrjSpeedNbValue = 0
 
