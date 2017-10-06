020cc9266bc4d0443cbf02c17899f022e45ea29d
Aethys256 <aethys256@users.noreply.github.com>
2017-10-06 19:30:25 +0200

    [Extract] Remove CreatureImmunities & Add Parsers

--- Extract_Live.bat
+++ Extract_Live.bat
@@ -14,10 +14,14 @@ set INPATH=..\..\AethysDBC\CDN\%PATCH%.%BUILD%\DBFilesClient
 set GTINPATH=..\..\AethysDBC\CDN\%PATCH%.%BUILD%\GameTables
 
 py -3 %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% -t csv Creature > %OUTPATH%\Creature.csv
-py -3 %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% -t csv CreatureImmunities > %OUTPATH%\CreatureImmunities.csv
 py -3 %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% -t csv CreatureType > %OUTPATH%\CreatureType.csv
 py -3 %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% -t csv ItemEffect > %OUTPATH%\ItemEffect.csv
 py -3 %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% -t csv Spell > %OUTPATH%\Spell.csv
 py -3 %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% -t csv SpellCooldowns > %OUTPATH%\SpellCooldowns.csv
 py -3 %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% -t csv SpellRange > %OUTPATH%\SpellRange.csv
 py -3 %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% -t csv SpellMisc > %OUTPATH%\SpellMisc.csv
+
+cd ..\..\AethysDBC
+py -3 Parse_TriggerGCD.py
+py -3 Parse_ProjectileSpeed.py
+py -3 Parse_ItemRange.py
