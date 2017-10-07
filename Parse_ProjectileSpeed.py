7036f2f6984cbcc7858be4393be26913a5b83723
Aethys256 <aethys256@users.noreply.github.com>
2017-10-07 21:21:24 +0200

    [Parse] Remove (in an ugly way) ending comma (to prevent nil values creation)

--- Parse_ProjectileSpeed.py
+++ Parse_ProjectileSpeed.py
@@ -32,15 +32,22 @@ PrjSpeedNbValue = 0
 
 with open(os.path.join(generatedDir, 'SpellMisc.csv')) as csvfile:
     reader = csv.DictReader(csvfile, escapechar='\\')
+    ValidRows = []
+    for row in reader:
+        if float(row['proj_speed']) > 0 and row['id'] in Spells:
+            ValidRows.append(row)
     with open(os.path.join(parsedDir, 'ProjectileSpeed.lua'), 'w', encoding='utf-8') as file:
         file.write('AethysCore.Enum.ProjectileSpeed = {\n')
-        for row in reader:
-            prj_speed_f = float(row['proj_speed'])
-            if prj_speed_f > 0 and row['id'] in Spells:
-                prj_speed_int = int(prj_speed_f)
-                PrjSpeedRawValue += prj_speed_int
-                PrjSpeedNbValue += 1
+        iMax = len(ValidRows)-1
+        for i, row in enumerate(ValidRows):
+            prj_speed_int = int(float(row['proj_speed']))
+            PrjSpeedRawValue += prj_speed_int
+            PrjSpeedNbValue += 1
+            if i == iMax:
+                file.write('  [' + Spells[row['id']] + '] = ' + str(prj_speed_int) + '\n')
+            else:
                 file.write('  [' + Spells[row['id']] + '] = ' + str(prj_speed_int) + ',\n')
         file.write('}\n')
 
+# Fun print
 print('Projectile Speed Mean : ' + str(PrjSpeedRawValue/PrjSpeedNbValue))
--- Parse_TriggerGCD.py
+++ Parse_TriggerGCD.py
@@ -16,9 +16,13 @@ parsedDir = os.path.join('DBC', 'parsed')
 os.chdir(os.path.join(os.path.dirname(sys.path[0]), 'AethysDBC'))
 
 with open(os.path.join(generatedDir, 'SpellCooldowns.csv')) as csvfile:
-    reader = csv.DictReader(csvfile)
+    reader = list(csv.DictReader(csvfile, escapechar='\\'))
     with open(os.path.join(parsedDir, 'TriggerGCD.lua'), 'w', encoding='utf-8') as file:
         file.write('AethysCore.Enum.TriggerGCD = {\n')
-        for row in reader:
-            file.write('  [' + row['id_spell'] + '] = ' + ('true' if int(row['gcd_cooldown']) > 0 else 'false') + ',\n')
+        iMax = len(reader)-1
+        for i, row in enumerate(reader):
+            if i == iMax:
+                file.write('  [' + row['id_spell'] + '] = ' + ('true' if int(row['gcd_cooldown']) > 0 else 'false') + '\n')
+            else:
+                file.write('  [' + row['id_spell'] + '] = ' + ('true' if int(row['gcd_cooldown']) > 0 else 'false') + ',\n')
         file.write('}\n')
