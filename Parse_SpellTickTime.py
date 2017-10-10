67812524edba291fe1637113a667aab09d27f3f1
Julien Rechaussat <Kutikuti@users.noreply.github.com>
2017-10-10 22:15:49 +0200

    [Parse] added hasted or not

--- Parse_SpellTickTime.py
+++ Parse_SpellTickTime.py
@@ -20,6 +20,9 @@ with open(os.path.join(generatedDir, 'SpellEffect.csv')) as csvfile:
     with open(os.path.join(parsedDir, 'TickTime.lua'), 'w', encoding='utf-8') as file:
         file.write('AethysCore.Enum.TickTime = {\n')
         for i, row in enumerate(reader):
-            if not row['amplitude'] == "0" :
-                file.write('  [' + row['id_spell'] + '] = ' + str(int(row['amplitude'])) + ',\n')
+            if not row['amplitude'] == "0" and not row['sp_coefficient'] == "0" :
+                if row['id_mechanic'] == "15" :#bleeds > non hasted
+                    file.write('  [' + row['id_spell'] + '] = {' + str(int(row['amplitude'])) + ',true},\n')
+                else:
+                    file.write('  [' + row['id_spell'] + '] = {' + str(int(row['amplitude'])) + ',false},\n')
         file.write('}\n')
