f80e86b60d1ab52c7c6f8b0a211245ecd79a089b
Aethys256 <aethys256@users.noreply.github.com>
2017-10-12 01:31:42 +0200

    [Parse] SpellTickTime refactor

--- Parse_SpellTickTime.py
+++ Parse_SpellTickTime.py
@@ -16,13 +16,17 @@ parsedDir = os.path.join('DBC', 'parsed')
 os.chdir(os.path.join(os.path.dirname(sys.path[0]), 'AethysDBC'))
 
 with open(os.path.join(generatedDir, 'SpellEffect.csv')) as csvfile:
-    reader = list(csv.DictReader(csvfile, escapechar='\\'))
+    reader = csv.DictReader(csvfile, escapechar='\\')
+    ValidRows = []
+    for row in reader:
+        if not row['amplitude'] == '0' and not row['sp_coefficient'] == '0':
+            ValidRows.append(row)
     with open(os.path.join(parsedDir, 'TickTime.lua'), 'w', encoding='utf-8') as file:
         file.write('AethysCore.Enum.TickTime = {\n')
-        for i, row in enumerate(reader):
-            if not row['amplitude'] == "0" and not row['sp_coefficient'] == "0" :
-                if row['id_mechanic'] == "15" :#bleeds > non hasted
-                    file.write('  [' + row['id_spell'] + '] = {' + str(int(row['amplitude'])) + ',false},\n')
-                else:
-                    file.write('  [' + row['id_spell'] + '] = {' + str(int(row['amplitude'])) + ',true},\n')
+        iMax = len(ValidRows)-1
+        for i, row in enumerate(ValidRows):
+            if i == iMax:
+                file.write('  [' + row['id_spell'] + '] = {' + str(int(row['amplitude'])) + ', ' + ('false' if row['id_mechanic'] == "15" else 'true') + '}\n')
+            else:
+                file.write('  [' + row['id_spell'] + '] = {' + str(int(row['amplitude'])) + ', ' + ('false' if row['id_mechanic'] == "15" else 'true') + '},\n')
         file.write('}\n')
