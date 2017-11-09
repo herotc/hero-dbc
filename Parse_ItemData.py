63510731cb7c492b10478eaab1a634a2e40d7ea3
Julien Rechaussat <Kutikuti@users.noreply.github.com>
2017-11-09 15:43:43 +0100

    [DBC] Updated legendary data with spec

--- Parse_ItemData.py
+++ Parse_ItemData.py
@@ -14,6 +14,8 @@ import json
 generatedDir = os.path.join('DBC', 'generated')
 parsedDir = os.path.join('DBC', 'parsed')
 
+classTable = {}
+
 os.chdir(os.path.join(os.path.dirname(sys.path[0]), 'AethysDBC'))
 
 def computeItemType(x):
@@ -40,6 +42,70 @@ def computeItemMaterial(x):
         8: 'leather',
         7: 'cloth'
     }.get(x, '')
+
+def createSpecTable():
+    global classTable
+    
+    # Death Knight
+    classTable['death_knight'] = {}
+    classTable['death_knight'][0] = 'blood'
+    classTable['death_knight'][1] = 'frost'
+    classTable['death_knight'][2] = 'unholy'
+    # Demon Hunter
+    classTable['demon_hunter'] = {}
+    classTable['demon_hunter'][0] = 'havoc'
+    classTable['demon_hunter'][1] = 'vengeance'
+    # Druid                    
+    classTable['druid'] = {}    
+    classTable['druid'][0] = 'balance'
+    classTable['druid'][1] = 'feral'
+    classTable['druid'][2] = 'guardian'
+    classTable['druid'][3] = 'restoration'
+    # Hunter 
+    classTable['hunter'] = {}
+    classTable['hunter'][0] = 'beast Mastery'
+    classTable['hunter'][1] = 'marksmanship'
+    classTable['hunter'][2] = 'survival'
+    # Mage 
+    classTable['mage'] = {}
+    classTable['mage'][0] = 'arcane'
+    classTable['mage'][1] = 'fire'
+    classTable['mage'][2] = 'frost'
+    # Monk 
+    classTable['monk'] = {}
+    classTable['monk'][0] = 'brewmaster'
+    classTable['monk'][1] = 'windwalker'
+    classTable['monk'][2] = 'mistweaver'
+    # Paladin 
+    classTable['paladin'] = {}
+    classTable['paladin'][0] = 'holy'
+    classTable['paladin'][1] = 'protection'
+    classTable['paladin'][2] = 'retribution'
+    # Priest 
+    classTable['priest'] = {}
+    classTable['priest'][0] = 'discipline'
+    classTable['priest'][1] = 'holy'
+    classTable['priest'][2] = 'shadow'
+    # Rogue 
+    classTable['rogue'] = {}
+    classTable['rogue'][0] = 'assassination'
+    classTable['rogue'][1] = 'outlaw'
+    classTable['rogue'][2] = 'subtlety'
+    # Shaman 
+    classTable['shaman'] = {}
+    classTable['shaman'][0] = 'elemental'
+    classTable['shaman'][1] = 'enhancement'
+    classTable['shaman'][2] = 'restoration'
+    # Warlock 
+    classTable['warlock'] = {}
+    classTable['warlock'][0] = 'affliction'
+    classTable['warlock'][1] = 'demonology'
+    classTable['warlock'][2] = 'destruction'
+    # Warrior 
+    classTable['warrior'] = {}
+    classTable['warrior'][0] = 'arms'
+    classTable['warrior'][1] = 'fury'
+    classTable['warrior'][2] = 'protection'
     
 def computeSet(set,ilvl):
     if set == 0:
@@ -152,6 +218,8 @@ def PrepareRow(row):
 
     return preparedRow
     
+# Program Start
+createSpecTable()
     
 with open(os.path.join(generatedDir, 'ItemSparse.csv')) as csvfile:
     reader = csv.DictReader(csvfile, escapechar='\\')
@@ -176,7 +244,7 @@ with open(os.path.join(generatedDir, 'ItemSparse.csv')) as csvfile:
                     ValidItemsRows[itemType] = []
                 ValidItemsRows[itemType].append(PrepareRow(row))
         
-        #Legendaries : baseilvl = 910      
+        #Legendaries : baseilvl = 910  
         if row['ilevel'] == '910' and int(row['quality']) == 5: 
             mask = computeLegClass(int(row['class_mask']))
             itemType = computeItemType(int(row['inv_type']))
@@ -186,30 +254,37 @@ with open(os.path.join(generatedDir, 'ItemSparse.csv')) as csvfile:
                 for i in range(len(t)):
                     if t[i] not in ValidLegendariesRows:
                         ValidLegendariesRows[t[i]] = {}
-                    if not itemType == "trinket" and not itemType == "neck" and not itemType == "finger" and not itemType == "back": #handle no materal separatly
-                        if itemType not in ValidLegendariesRows[t[i]]:
-                            ValidLegendariesRows[t[i]][itemType] = {} #Dictionnary because we need to order more
-                        if itemMaterial not in ValidLegendariesRows[t[i]][itemType]:
-                            ValidLegendariesRows[t[i]][itemType][itemMaterial] = [] #List of item ordered
-                        ValidLegendariesRows[t[i]][itemType][itemMaterial].append(PrepareRow(row))
-                    else:
-                        if itemType not in ValidLegendariesRows[t[i]]:
-                            ValidLegendariesRows[t[i]][itemType] = [] #Dictionnary because we need to order more
-                        ValidLegendariesRows[t[i]][itemType].append(PrepareRow(row))
+                    current_class = '' + t[i]
+                    for j in range(len(classTable[current_class])):  
+                        if classTable[current_class][j] not in ValidLegendariesRows[t[i]]:
+                            ValidLegendariesRows[t[i]][classTable[t[i]][j]] = {}    
+                        if not itemType == "trinket" and not itemType == "neck" and not itemType == "finger" and not itemType == "back": #handle no materal separatly
+                            if itemType not in ValidLegendariesRows[t[i]][classTable[t[i]][j]]:
+                                ValidLegendariesRows[t[i]][classTable[t[i]][j]][itemType] = {} #Dictionnary because we need to order more
+                            if itemMaterial not in ValidLegendariesRows[t[i]][classTable[t[i]][j]][itemType]:
+                                ValidLegendariesRows[t[i]][classTable[t[i]][j]][itemType][itemMaterial] = [] #List of item ordered
+                            ValidLegendariesRows[t[i]][classTable[t[i]][j]][itemType][itemMaterial].append(PrepareRow(row))
+                        else:
+                            if itemType not in ValidLegendariesRows[t[i]][classTable[t[i]][j]]:
+                                ValidLegendariesRows[t[i]][classTable[t[i]][j]][itemType] = [] #Dictionnary because we need to order more
+                            ValidLegendariesRows[t[i]][classTable[t[i]][j]][itemType].append(PrepareRow(row))
 
             else: 
-                if mask not in ValidLegendariesRows:
+                for j in range(len(classTable[mask])):
+                    if mask not in ValidLegendariesRows:
                         ValidLegendariesRows[mask] = {}
-                if not itemType == "trinket" and not itemType == "neck" and not itemType == "finger" and not itemType == "back": #handle no materal separatly
-                    if itemType not in ValidLegendariesRows[mask]:
-                        ValidLegendariesRows[mask][itemType] = {} #Dictionnary because we need to order more
-                    if itemMaterial not in ValidLegendariesRows[mask][itemType]:
-                        ValidLegendariesRows[mask][itemType][itemMaterial] = [] #List of item ordered
-                    ValidLegendariesRows[mask][itemType][itemMaterial].append(PrepareRow(row))
-                else:
-                    if itemType not in ValidLegendariesRows[mask]:
-                        ValidLegendariesRows[mask][itemType] = [] #Dictionnary because we need to order more
-                    ValidLegendariesRows[mask][itemType].append(PrepareRow(row))
+                    if classTable[mask][j] not in ValidLegendariesRows[mask]:
+                        ValidLegendariesRows[mask][classTable[mask][j]] = {}
+                    if not itemType == "trinket" and not itemType == "neck" and not itemType == "finger" and not itemType == "back": #handle no materal separatly
+                        if itemType not in ValidLegendariesRows[mask][classTable[mask][j]]:
+                            ValidLegendariesRows[mask][classTable[mask][j]][itemType] = {} #Dictionnary because we need to order more
+                        if itemMaterial not in ValidLegendariesRows[mask][classTable[mask][j]][itemType]:
+                            ValidLegendariesRows[mask][classTable[mask][j]][itemType][itemMaterial] = [] #List of item ordered
+                        ValidLegendariesRows[mask][classTable[mask][j]][itemType][itemMaterial].append(PrepareRow(row))
+                    else:
+                        if itemType not in ValidLegendariesRows[mask][classTable[mask][j]]:
+                            ValidLegendariesRows[mask][classTable[mask][j]][itemType] = [] #Dictionnary because we need to order more
+                        ValidLegendariesRows[mask][classTable[mask][j]][itemType].append(PrepareRow(row))
           
     #Prints everything to the files  
     with open(os.path.join(parsedDir, 'generatorItemData.json'), 'w', encoding='utf-8') as file:
