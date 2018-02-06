878643f9552c28a3e216e846f16d931dfb8538ed
Julien Rechaussat <Kutikuti@users.noreply.github.com>
2018-02-06 16:32:37 +0100

    [DBC] Fix ProjectileSpeed with dbc change

--- Parse_ProjectileSpeed.py
+++ Parse_ProjectileSpeed.py
@@ -16,15 +16,7 @@ parsedDir = os.path.join('DBC', 'parsed')
 os.chdir(os.path.join(os.path.dirname(sys.path[0]), 'AethysDBC'))
 
 ## Mapping
-# id_spell & id_misc from Spell
-# id_misc & proj_speed from SpellMisc
-
-# Get every misc id associated to every spell id
-Spells = {}
-with open(os.path.join(generatedDir, 'Spell.csv')) as csvfile:
-    reader = csv.DictReader(csvfile, escapechar='\\')
-    for row in reader:
-        Spells[row['id_misc']] = row['id']
+# id_parent & proj_speed from SpellMisc
 
 # Variables to compute the Projectile Speed Mean, for fun =)
 PrjSpeedRawValue = 0
@@ -34,7 +26,7 @@ with open(os.path.join(generatedDir, 'SpellMisc.csv')) as csvfile:
     reader = csv.DictReader(csvfile, escapechar='\\')
     ValidRows = []
     for row in reader:
-        if float(row['proj_speed']) > 0 and row['id'] in Spells:
+        if float(row['proj_speed']) > 0:
             ValidRows.append(row)
     with open(os.path.join(parsedDir, 'ProjectileSpeed.lua'), 'w', encoding='utf-8') as file:
         file.write('AethysCore.Enum.ProjectileSpeed = {\n')
@@ -44,9 +36,9 @@ with open(os.path.join(generatedDir, 'SpellMisc.csv')) as csvfile:
             PrjSpeedRawValue += prj_speed_int
             PrjSpeedNbValue += 1
             if i == iMax:
-                file.write('  [' + Spells[row['id']] + '] = ' + str(prj_speed_int) + '\n')
+                file.write('  [' + row['id_parent'] + '] = ' + str(prj_speed_int) + '\n')
             else:
-                file.write('  [' + Spells[row['id']] + '] = ' + str(prj_speed_int) + ',\n')
+                file.write('  [' + row['id_parent'] + '] = ' + str(prj_speed_int) + ',\n')
         file.write('}\n')
 
 # Fun print
