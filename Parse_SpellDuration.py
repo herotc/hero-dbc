abf9c8d5387939fce590c21395883a51ca275a3b
Julien Rechaussat <Kutikuti@users.noreply.github.com>
2018-02-06 16:40:38 +0100

    [DBC] Fix SpellDuration with dbc change

--- Parse_SpellDuration.py
+++ Parse_SpellDuration.py
@@ -16,16 +16,8 @@ parsedDir = os.path.join('DBC', 'parsed')
 os.chdir(os.path.join(os.path.dirname(sys.path[0]), 'AethysDBC'))
 
 ## Mapping
-# id_spell & id_misc from Spell
-# id_misc & proj_speed from SpellMisc
-
-# Get every misc id associated to every spell id
-Spells = {}
-with open(os.path.join(generatedDir, 'Spell.csv')) as csvfile:
-    reader = csv.DictReader(csvfile, escapechar='\\')
-    for row in reader:
-        Spells[row['id_misc']] = row['id']
-        
+# id_parent & proj_speed from SpellMisc
+       
 Durations = {}
 with open(os.path.join(generatedDir, 'SpellDuration.csv')) as csvfile:
     reader = csv.DictReader(csvfile, escapechar='\\')
@@ -39,7 +31,7 @@ with open(os.path.join(generatedDir, 'SpellMisc.csv')) as csvfile:
     reader = csv.DictReader(csvfile, escapechar='\\')
     ValidRows = []
     for row in reader:
-        if int(row['id_duration']) > 0 and row['id'] in Spells and row['id_duration'] in Durations:
+        if int(row['id_duration']) > 0 and row['id_duration'] in Durations:
             ValidRows.append(row)
     with open(os.path.join(parsedDir, 'SpellDuration.lua'), 'w', encoding='utf-8') as file:
         file.write('AethysCore.Enum.SpellDuration = {\n')
@@ -49,9 +41,9 @@ with open(os.path.join(generatedDir, 'SpellMisc.csv')) as csvfile:
             pandemic = int(float(Durations[row['id_duration']][1])*0.3)
             maxDuration = baseDuration + pandemic
             if i == iMax:
-                file.write('  [' + Spells[row['id']] + '] = {' + Durations[row['id_duration']][1] + ', ' + str(int(maxDuration)) + '}\n')
+                file.write('  [' + row['id_parent'] + '] = {' + Durations[row['id_duration']][1] + ', ' + str(int(maxDuration)) + '}\n')
             else:
-                file.write('  [' + Spells[row['id']] + '] = {' + Durations[row['id_duration']][1] + ', ' + str(int(maxDuration)) + '},\n')
+                file.write('  [' + row['id_parent'] + '] = {' + Durations[row['id_duration']][1] + ', ' + str(int(maxDuration)) + '},\n')
         file.write('}\n')
 
 
