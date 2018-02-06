b0e06e8a979baac6eea4134c1ae16dddfcf15363
Julien Rechaussat <Kutikuti@users.noreply.github.com>
2018-02-06 16:45:51 +0100

    [DBC] Fix SpellMeleeRange with dbc change

--- Parse_SpellMeleeRange.py
+++ Parse_SpellMeleeRange.py
@@ -16,15 +16,8 @@ parsedDir = os.path.join('DBC', 'parsed')
 os.chdir(os.path.join(os.path.dirname(sys.path[0]), 'AethysDBC'))
 
 ## Mapping
-# id_spell & id_misc from Spell
 # id_misc & proj_speed from SpellMisc
 
-# Get every misc id associated to every spell id
-Spells = {}
-with open(os.path.join(generatedDir, 'Spell.csv')) as csvfile:
-    reader = csv.DictReader(csvfile, escapechar='\\')
-    for row in reader:
-        Spells[row['id_misc']] = row['id']
 # Get every 'valid' ranges for our parser.
 # We ignore the '_2' ranges (friendly) and only considers the '_1' ones (hostile)
 # We also considers only the range with the flag '1' that identify them as melee.
@@ -42,14 +35,14 @@ with open(os.path.join(generatedDir, 'SpellMisc.csv')) as csvfile:
     reader = csv.DictReader(csvfile, escapechar='\\')
     ValidRows = []
     for row in reader:
-        if row['id'] in Spells and row['id_range'] in Ranges:
+        if row['id_range'] in Ranges:
             ValidRows.append(row)
     with open(os.path.join(parsedDir, 'SpellMeleeRange.lua'), 'w', encoding='utf-8') as file:
         file.write('AethysCore.Enum.SpellMeleeRange = {\n')
         iMax = len(ValidRows)-1
         for i, row in enumerate(ValidRows):
             if i == iMax:
-                file.write('  [' + Spells[row['id']] + '] = {' + ('true' if Ranges[row['id_range']][2] == 1 else 'false') + ', ' + Ranges[row['id_range']][0] + ', ' + Ranges[row['id_range']][1] +'}\n')
+                file.write('  [' + row['id_parent'] + '] = {' + ('true' if Ranges[row['id_range']][2] == 1 else 'false') + ', ' + Ranges[row['id_range']][0] + ', ' + Ranges[row['id_range']][1] +'}\n')
             else:
-                file.write('  [' + Spells[row['id']] + '] = {' + ('true' if Ranges[row['id_range']][2] == 1 else 'false') + ', ' + Ranges[row['id_range']][0] + ', ' + Ranges[row['id_range']][1] +'},\n')
+                file.write('  [' + row['id_parent'] + '] = {' + ('true' if Ranges[row['id_range']][2] == 1 else 'false') + ', ' + Ranges[row['id_range']][0] + ', ' + Ranges[row['id_range']][1] +'},\n')
         file.write('}\n')
