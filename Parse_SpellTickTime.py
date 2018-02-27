df5cd736d1966a8640de0769bdcf930a0b974bd4
Julien Rechaussat <Kutikuti@users.noreply.github.com>
2018-02-27 12:28:43 +0100

    [DBC] Fix TickTime for real
    and remove prints

--- Parse_SpellTickTime.py
+++ Parse_SpellTickTime.py
@@ -19,16 +19,14 @@ with open(os.path.join(generatedDir, 'SpellEffect.csv')) as csvfile:
     reader = csv.DictReader(csvfile, escapechar='\\')
     ValidRows = []
     for row in reader:
-        if not float(row['amplitude']) == 0 and not float(row['sp_coefficient']) == 0:
+        if not float(row['amplitude']) == 0:
             ValidRows.append(row)
     with open(os.path.join(parsedDir, 'TickTime.lua'), 'w', encoding='utf-8') as file:
         file.write('AethysCore.Enum.TickTime = {\n')
         iMax = len(ValidRows)-1
         for i, row in enumerate(ValidRows):
             if i == iMax:
-                print(row['amplitude'])
-                file.write('  [' + row['id_parent'] + '] = {' + str(float(row['amplitude'])) + ', ' + ('false' if row['id_mechanic'] == "15" else 'true') + '}\n')
+                file.write('  [' + row['id_parent'] + '] = {' + str(int(float(row['amplitude']))) + ', ' + ('false' if row['id_mechanic'] == "15" else 'true') + '}\n')
             else:
-                print(row['amplitude'])
-                file.write('  [' + row['id_parent'] + '] = {' + str(float(row['amplitude'])) + ', ' + ('false' if row['id_mechanic'] == "15" else 'true') + '},\n')
+                file.write('  [' + row['id_parent'] + '] = {' + str(int(float(row['amplitude']))) + ', ' + ('false' if row['id_mechanic'] == "15" else 'true') + '},\n')
         file.write('}\n')
