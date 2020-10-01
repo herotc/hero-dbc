f3921cf3d3a11ce65f32d968b3e4aa1efa7a75a8
Julien Rechaussat <Kutikuti@users.noreply.github.com>
2020-10-01 13:30:00 +0200

    fix: [Parser] SpellGCD: sort list

--- scripts/parsers/SpellGCD.py
+++ scripts/parsers/SpellGCD.py
@@ -17,6 +17,7 @@ os.chdir(os.path.join(os.path.dirname(sys.path[0]), '..', '..', 'hero-dbc'))
 
 with open(os.path.join(generatedDir, 'SpellCooldowns.csv')) as csvfile:
     reader = list(csv.DictReader(csvfile, escapechar='\\'))
+    reader = sorted(reader, key=lambda d: int(d['id_parent']))
     with open(os.path.join(addonEnumDir, 'SpellGCD.lua'), 'w', encoding='utf-8') as file:
         file.write('HeroDBC.DBC.SpellGCD = {\n')
         for _, row in enumerate(reader):
