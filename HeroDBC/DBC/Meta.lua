d7852ddb435cdb1de68804267a6e01497bb5f9e7
Cilraaz <cilraaz@gmail.com>
2024-08-14 11:20:28 -0400

    chore(DBC): 11.0.2.56110 data

--- HeroDBC/DBC/Meta.lua
+++ HeroDBC/DBC/Meta.lua
@@ -1,2 +1,2 @@
-HeroDBC.DBC.metaVersion = "11.0.0.55824"
-HeroDBC.DBC.metaTime = "2024-07-29T14:51:09.405417"
+HeroDBC.DBC.metaVersion = "11.0.2.56110"
+HeroDBC.DBC.metaTime = "2024-08-13T16:00:25.939104"
