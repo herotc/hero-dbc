fc691001e4f36d27babd0b3559b8884f624c7d42
Joshua <37181202+joshjau@users.noreply.github.com>
2025-03-02 07:41:39 +0800

    v11.1.0.59466

--- HeroDBC/DBC/Meta.lua
+++ HeroDBC/DBC/Meta.lua
@@ -1,2 +1,2 @@
-HeroDBC.DBC.metaVersion = "11.0.7.58162"
-HeroDBC.DBC.metaTime = "2024-12-17T19:13:49.769561"
+HeroDBC.DBC.metaVersion = "11.1.0.59466"
+HeroDBC.DBC.metaTime = "2025-03-02T06:02:05.871004"
--- HeroDBC/DBC/SpellConduits.lua
+++ HeroDBC/DBC/SpellConduits.lua
@@ -1,4 +1,4 @@
--- Generated using WoW 10.2.5.53262 client data on 2024-02-14T09:41:30.867867.
+-- Generated using WoW 11.1.0.59466 client data on 2025-03-02T06:02:05.728014.
 HeroDBC.DBC.SpellConduits = {
   [5] = 334993,
   [7] = 335010,
