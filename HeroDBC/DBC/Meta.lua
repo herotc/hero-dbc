f9ff051904aad23ff71bc6ad1161df6f18d229b2
JoshAU <37181202+joshjau@users.noreply.github.com>
2025-04-15 23:39:19 +0800

    chore(DBC): 11.1.0.60257 data (#17)

--- HeroDBC/DBC/Meta.lua
+++ HeroDBC/DBC/Meta.lua
@@ -1,2 +1,2 @@
-HeroDBC.DBC.metaVersion = "11.1.0.60189"
-HeroDBC.DBC.metaTime = "2025-04-09T08:09:21.705670"
+HeroDBC.DBC.metaVersion = "11.1.0.60257"
+HeroDBC.DBC.metaTime = "2025-04-12T06:51:39.927548"
--- HeroDBC/DBC/SpellAuraStat.lua
+++ HeroDBC/DBC/SpellAuraStat.lua
@@ -1,4 +1,4 @@
--- Generated using WoW 11.1.0.60189 client data on 2025-04-09T08:09:21.519787.
+-- Generated using WoW 11.1.0.60257 client data on 2025-04-12T06:51:39.799521.
 HeroDBC.DBC.SpellAuraStat = {
   [13] = false,
   [17] = false,
@@ -165305,4 +165305,5 @@ HeroDBC.DBC.SpellAuraStat = {
   [1234963] = false,
   [1235399] = false,
   [1235400] = false,
+  [1236977] = false,
 }
