688d8008d4303f98e7553ef40bac2c4ffd26b408
JoshAU <37181202+joshjau@users.noreply.github.com>
2025-09-09 23:32:14 +0800

    chore(DBC): 11.2.0.63003 data

--- HeroDBC/DBC/Meta.lua
+++ HeroDBC/DBC/Meta.lua
@@ -1,2 +1,2 @@
-HeroDBC.DBC.metaVersion = "11.2.0.62876"
-HeroDBC.DBC.metaTime = "2025-08-30T12:31:49.243582"
+HeroDBC.DBC.metaVersion = "11.2.0.63003"
+HeroDBC.DBC.metaTime = "2025-09-05T08:36:17.952812"
